11f664a8a9825bd9a68de7b323a352867e1a1fe9
Florent <florent.guyard@live.fr>
2024-06-01 13:22:17 +0200

    correctif plugin

--- index.js
+++ index.js
@@ -1,22 +1,64 @@
 "use strict";
 
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-var _react = _interopRequireDefault(require("react"));
-var _cross = _interopRequireDefault(require("../assets/cross.svg"));
+var _react = _interopRequireWildcard(require("react"));
+var _cross = _interopRequireDefault(require("./assets/cross.png"));
+require("./style/modal.css");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var Modal = function Modal() {
+function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
+function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != _typeof(e) && "function" != typeof e) return { "default": e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n["default"] = e, t && t.set(e, n), n; }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
+function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t["return"] && (u = t["return"](), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+var Modal = function Modal(_ref) {
+  var isOpen = _ref.isOpen,
+    _ref$textContent = _ref.textContent,
+    textContent = _ref$textContent === void 0 ? "rgvfds!" : _ref$textContent,
+    _ref$width = _ref.width,
+    width = _ref$width === void 0 ? "50%" : _ref$width,
+    _ref$height = _ref.height,
+    height = _ref$height === void 0 ? "10%" : _ref$height;
+  var _useState = (0, _react.useState)(true),
+    _useState2 = _slicedToArray(_useState, 2),
+    isModalOpen = _useState2[0],
+    setIsModalOpen = _useState2[1];
+  var handleClosure = function handleClosure(event) {
+    if (event.target === modalRef.current) {
+      var modalToClose = document.querySelector('.modal');
+      modalToClose.style.display = "none";
+      setIsModalOpen(false);
+    }
+  };
+  var handleClosureCross = function handleClosureCross() {
+    var modalToClose = document.querySelector('.modal');
+    modalToClose.style.display = "none";
+    setIsModalOpen(false);
+  };
+  var modalRef = (0, _react.useRef)();
   return /*#__PURE__*/_react["default"].createElement("section", {
-    className: "modal"
+    className: "modal",
+    ref: modalRef,
+    onClick: handleClosure
   }, /*#__PURE__*/_react["default"].createElement("div", {
-    className: "modal_div"
+    className: "modal_div",
+    style: {
+      width: width,
+      height: height
+    }
   }, /*#__PURE__*/_react["default"].createElement("p", {
     className: "modal_div_paragraph"
-  }, "Ici il y a une modale"), /*#__PURE__*/_react["default"].createElement("img", {
+  }, textContent), /*#__PURE__*/_react["default"].createElement("img", {
+    onClick: handleClosureCross,
     className: "modal_div_cross",
-    src: _cross["default"]
+    src: _cross["default"],
+    alt: "cross icon"
   })));
 };
 var _default = exports["default"] = Modal;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import React from "react";
 import CrossIcon from "../assets/cross.png";
 import "../style/modal.css";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
-const Modal = () => {
+const Modal = ({ isOpen, textContent = "employee Created !!", width = "50%", height = "10%"}) => {
+
+    const [isModalOpen, setIsModalOpen] = useState(true)
 
     
-
-    const textContent = "Employee Created!";
 
     const handleClosure = (event) => {
         if (event.target === modalRef.current) {
           const modalToClose = document.querySelector('.modal');
           modalToClose.style.display = "none";
+          setIsModalOpen(false)
         }
     }
 
     const handleClosureCross = () => {
-      const modalToClose = document.querySelector('.modal');
-      modalToClose.style.display = "none";
+        const modalToClose = document.querySelector('.modal');
+        modalToClose.style.display = "none";
+        setIsModalOpen(false)
     }
 
     const modalRef = useRef()
@@ -26,7 +28,7 @@ const Modal = () => {
     return (
 
         <section className="modal" ref={modalRef} onClick={handleClosure}>
-            <div className="modal_div">
+            <div className="modal_div" style={{ width, height }}>
                 <p className="modal_div_paragraph">{textContent}</p>
                 <img onClick={handleClosureCross} className="modal_div_cross" src={CrossIcon} alt="cross icon"/>
             </div>
